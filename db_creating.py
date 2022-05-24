433b05fffa2ef58d592f047ff9c610e6f267a589
DavidBobek <55330836+DavidBobek@users.noreply.github.com>
2022-05-24 22:37:49 +0200

    master

--- db_creating.py
+++ db_creating.py
@@ -1,40 +1,40 @@
 import psycopg2
+def initialize():
+    con = psycopg2.connect(
+        host = 'localhost',
+        database='GreatBuy',
+        user = 'postgres',
+        password = 'postgres',
 
-con = psycopg2.connect(
-    host = 'localhost',
-    database='GreatBuy',
-    user = 'postgres',
-    password = 'postgres',
-
-    
-    
-)
-
-
-
-sql_query='''
-
-    CREATE TABLE UserData(
-        USER_ID TEXT PRIMARY KEY NOT NULL,
-        PASSWORD TEXT,
-        EMAIL TEXT
-        NAME TEXT
         
         
-    ); 
-
-'''
-
-pointer = con.cursor()
+    )
 
 
-try:
-    pointer.execute(sql_query)
-    con.commit()
-    print("Table created")
-    
-except:
-    print("already created")
 
-finally:
-    con.close()
+    sql_query='''
+
+        CREATE TABLE UserData(
+            USER_ID TEXT PRIMARY KEY NOT NULL,
+            PASSWORD TEXT,
+            EMAIL TEXT
+            NAME TEXT
+            
+            
+        ); 
+
+    '''
+
+    pointer = con.cursor()
+
+
+    try:
+        pointer.execute(sql_query)
+        con.commit()
+        print("Table created")
+        
+    except:
+        print("Table already created")
+
+    finally:
+        con.close()
