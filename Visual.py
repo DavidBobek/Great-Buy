3d908b90693d8d0fcc3e422f51078bbf816b5522
DavidBobek <55330836+DavidBobek@users.noreply.github.com>
2022-05-28 23:35:51 +0200

    master

--- Visual.py
+++ Visual.py
@@ -26,6 +26,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
+       
+        
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(500, 749)
         MainWindow.setStyleSheet("background-color:rgb(247, 255, 240)")
